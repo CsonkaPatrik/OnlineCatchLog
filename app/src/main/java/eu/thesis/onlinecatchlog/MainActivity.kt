8631c3524cf0123333df280fc79a59ad9a256630
Csonka Patrik <patx5see@gmail.com>
2024-10-25 12:30:04 +0200

    first version, splash screen adaptation

---
+++ app/src/main/java/eu/thesis/onlinecatchlog/MainActivity.kt
@@ -0,0 +1,38 @@
+package eu.thesis.onlinecatchlog
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import eu.thesis.onlinecatchlog.ui.theme.OnlineCatchLogTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Handle the splash screen transition.
+        val splashScreen = installSplashScreen()
+
+        
+
+        super.onCreate(savedInstanceState)
+        setContent {
+            OnlineCatchLogTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+
+                }
+            }
+        }
+    }
+}
