003b089702748ecbd01724c7073abfe6f2ab3ebc
CsonkaPatrik <patx5see@gmail.com>
2024-11-04 16:21:15 +0100

    minor fix

--- app/src/main/java/eu/thesis/onlinecatchlog/screens/main/MainViewModel.kt
+++ app/src/main/java/eu/thesis/onlinecatchlog/screens/main/MainViewModel.kt
@@ -1,14 +1,9 @@
 package eu.thesis.onlinecatchlog.screens.main
 
-import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import eu.thesis.onlinecatchlog.Screen
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 open class MainViewModel:ViewModel() {
 
