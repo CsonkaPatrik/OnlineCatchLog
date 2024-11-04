bab21a66c5fa599c54a9677d431d46d961900324
CsonkaPatrik <patx5see@gmail.com>
2024-11-04 16:17:13 +0100

    New splash screen (loading screen)

---
+++ app/src/main/java/eu/thesis/onlinecatchlog/screens/splash/SplashViewModel.kt
@@ -0,0 +1,20 @@
+package eu.thesis.onlinecatchlog.screens.splash
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import eu.thesis.onlinecatchlog.MAIN_VIEW_SCREEN
+import eu.thesis.onlinecatchlog.SIGN_IN_SCREEN
+import eu.thesis.onlinecatchlog.SPLASH_SCREEN
+import eu.thesis.onlinecatchlog.model.service.AccountService
+import eu.thesis.onlinecatchlog.screens.MainAppViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(
+    private val accountService: AccountService
+) : MainAppViewModel() {
+
+    fun onAppStart(openAndPopUp: (String, String) -> Unit) {
+        if (accountService.hasUser()) openAndPopUp(MAIN_VIEW_SCREEN, SPLASH_SCREEN)
+        else openAndPopUp(SIGN_IN_SCREEN, SPLASH_SCREEN)
+    }
+}
