8250774fb100244df6f5d544f2e4485551856be6
CsonkaPatrik <patx5see@gmail.com>
2024-11-09 13:50:50 +0100

    minor fixes, adding routes

--- app/src/main/java/eu/thesis/onlinecatchlog/MainApp.kt
+++ app/src/main/java/eu/thesis/onlinecatchlog/MainApp.kt
@@ -6,13 +6,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import eu.thesis.onlinecatchlog.screens.account.AccountView
+import eu.thesis.onlinecatchlog.screens.catchlog.CatchLogView
+import eu.thesis.onlinecatchlog.screens.catchlog.CatchLogViewModel
 import eu.thesis.onlinecatchlog.screens.login.LoginView
 import eu.thesis.onlinecatchlog.screens.main.MainView
 import eu.thesis.onlinecatchlog.screens.splash.SplashScreen
@@ -55,5 +61,13 @@ fun NavGraphBuilder.notesGraph(appState: MainAppState) {
     }
     composable(SPLASH_SCREEN){
         SplashScreen(openAndPopUp = { route, popUp -> appState.navigateAndPopUp(route, popUp) })
+    }
+    composable(ACCOUNT_SCREEN){
+        AccountView(restartApp = { route -> appState.clearAndNavigate(route) })
+    }
+    composable(CATCHLOG_SCREEN) {
+        val viewModel: CatchLogViewModel = hiltViewModel()
+        val state by viewModel.state.collectAsState()
+        CatchLogView(state = state, onEvent = viewModel::onEvent)
     }
 }
